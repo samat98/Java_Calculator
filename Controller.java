ba3a7271932b1bbbfc0aa124bdc75793e2f22d1f
samat98 <samat989a@gmail.com>
2020-08-08 00:24:07 +0600

    deleted whitespace splitting and added splitting depend operator and fixed some issues

--- Controller.java
+++ Controller.java
@@ -3,8 +3,23 @@ public class Controller{
     View view = new View();
 
     public Controller(){
-        String[] expression = view.getInputText().split(" ", 0);
+        String inputText = view.getInputText();
+        String[] expression = splittedText(inputText);
         Model model = new Model(expression);
         view.printResult(model.getRes());
+    }
+
+    String[] splittedText(String str){
+        String[] res = new String[3];
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(c == '+' || c == '-' || c == '*' || c == '/') {
+                res[0] = str.substring(0, i).trim();
+                res[1] = String.valueOf(c);
+                res[2] = str.substring(i+1).trim();
+                return res;
+            }
+        }
+        return res;
     }
 }
--- Model.java
+++ Model.java
@@ -37,12 +37,13 @@ public class Model{
         }
     }
     private String withArabic(String[] x) throws HandledException {
+        //если больше чем 2 переменных и 1 оператора
         if(x.length != 3) throw new HandledException("withArabic", "Must be two variables with operations: +,-,/,* and seperated with whitespace");
         try {
             int c1 = Integer.parseInt(x[0]);
             String op = x[1];
             int c2 = Integer.parseInt(x[2]);
-            if (c1 > 10 || c2 > 10) throw new HandledException("withArabic", "must be less 10");
+            if (c1 > 10 || c2 > 10 || c1 < 1 || c2 < 1) throw new HandledException("withArabic", "must be less 10 and greeter than 1");
             else {
                 return  Integer.toString(calc(c1, op, c2));
             }
@@ -50,33 +51,31 @@ public class Model{
         catch (Exception e){
             System.out.println(e);
         }
-        return "0";
+        return "something wrong";
     }
     private String withRoman(String[] x) throws HandledException {
+        // Мап со значениями от 1 до 10
         HashMap<String, String> map=new HashMap<String, String>();
-        map.put("", "0");
-        map.put("I", "1");
-        map.put("II", "2");
-        map.put("III", "3");
-        map.put("IV", "4");
-        map.put("V", "5");
-        map.put("VI", "6");
-        map.put("VII", "7");
-        map.put("VIII", "8");
-        map.put("IX", "9");
-        map.put("X", "10");
-        map.put("C", "100");
+        map.put("", "0");map.put("I", "1");map.put("II", "2");
+        map.put("III", "3");map.put("IV", "4");map.put("V", "5");
+        map.put("VI", "6");map.put("VII", "7");map.put("VIII", "8");
+        map.put("IX", "9");map.put("X", "10");map.put("C", "100");//и 100))
 
         //I обозначает 1, V обозначает 5, X — 10, L — 50, C — 100
         if(map.containsKey(x[0]) && map.containsKey(x[2])){
             x[0] = map.get(x[0]);
             x[2] = map.get(x[2]);
             String temp = withArabic(x);
-            if (temp.length() == 2){
+            if(Integer.parseInt(temp) < 0) return "-" + getKey(map, temp.substring(1));
+            else if (temp.length() == 2){
                 String secondChar = getKey(map, String.valueOf(temp.charAt(1)));
+                //X = I, L = V, C = X в десятичных 99 = (IX)IX = XCIX
                 String firstChar = getKey(map, String.valueOf(temp.charAt(0))).replace("X","C").replace("V", "L").replace("I", "X");
                 return firstChar + secondChar;
-            }else return getKey(map, temp);
+            }
+            //0 на римском Nulla
+            else if(Integer.parseInt(temp) == 0) return "nulla";
+            else return getKey(map, temp);
         }
         else throw new HandledException("withRoman", "must be proper roman characters");
 
